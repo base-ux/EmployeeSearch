682972338d81f963613c372ba69517c46c0df9f9
Toni Silfver <toni.silfver94@gmail.com>
2020-09-12 18:09:53 +0300

    v61

--- src/main/resources/public/javascript/code.js
+++ src/main/resources/public/javascript/code.js
@@ -1,10 +1,3 @@
 $(".carousel").carousel({
   interval: 10000
 })
-
-
-
-var curve = $(".welcomeLeftBarCurve")
-var upperCurve = curve.getContext("2d");
-upperCurve.fillStyle = "#FF0000";
-upperCurve.fillRect(0,0,150,75);
