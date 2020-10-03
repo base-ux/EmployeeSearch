3ad3bc132051ef7cf49bf2514b6e9b72799a5187
Toni Silfver <toni.silfver94@gmail.com>
2020-10-03 09:20:40 +0300

    v134

--- src/main/resources/public/javascript/code.js
+++ src/main/resources/public/javascript/code.js
@@ -6,37 +6,36 @@ $(document).ready(function(){
         var $signupButton = $("#signup")
         var $passwordField = $("#RegisterFormPassword")
         var $confirmPasswordField = $("#RegisterFormMatchingPassword")
-        var $errorMessage = $('<div class="container-fluid"><small class="form-text mt-4 text-center text-danger" id="error">confirm must match password</small></div>')
+        var $errorMessage = $('<small class="container-fluid form-text mt-4 text-center text-danger" id="error">confirm must match password</small>')
 
-        $signupButton.removeAttr("disabled")
-
-        function checkMatchingPasswords(){
-            if($confirmPasswordField.val() != "" && $passwordField.val != ""){
-                if($confirmPasswordField.val() != $passwordField.val()){
-                    $signupButton.attr("disabled", "disabled")
+        function checkMatchingPassword(){
+            if($confirmPasswordField.val() != $passwordField.val() && 
+            $confirmPasswordField.val() != "" && $passwordField.val() != ""){
                     $errorMessage.insertAfter($confirmPasswordField)
-                }
+                    $signupButton.attr("disabled", "disabled")
             }
         }
 
-        function resetMatchingPasswordsError(){
+        function resetMatchingPasswordError(){
             $signupButton.removeAttr("disabled")
             var $errorMessageShow = $("#error")
             if($errorMessageShow.length > 0){
                 $errorMessageShow.remove()
-            } 
+            }
         }
 
         $("#RegisterFormMatchingPassword, #RegisterFormPassword")
-            .on("keydown", function(e){
-                if(e.keyCode == 13 || e.keyCode == 9){
-                    checkMatchingPasswords()
+            .on("keydown", function(err){
+                if(err.keyCode == 13 || err.keyCode == 9){
+                    checkMatchingPassword()
                 }
             })
             .on("blur", function(){
-                checkMatchingPasswords()
+                checkMatchingPassword()
             })
             .on("focus", function(){
-                resetMatchingPasswordsError()
+                resetMatchingPasswordError()
             })
+
+        $signupButton.removeAttr("disabled")
 })
